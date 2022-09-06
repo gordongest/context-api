cc19a77a5115281195e34369cb426a4b87b83e1e
Gordon Gest <gordon.gest@gmail.com>
2022-09-06 12:58:02 -0600

    add form component

--- src/Form.jsx
+++ src/Form.jsx
@@ -17,9 +17,39 @@ import { LockOutlined } from "@mui/icons-material";
 import { withStyles } from "@mui/styles";
 import formStyles from "./styles/FormStyles";
 
-const Form = ({ classes }) => (
-  <main className={classes.main}>
-    <h1>form</h1>
-  </main>
-);
+const Form = ({ classes }) =>
+    <main className={classes.main}>
+        <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+                <LockOutlined />
+            </Avatar>
+            <Typography variant="h5">Sign In</Typography>
+            <Select value="english">
+                <MenuItem value="english">English</MenuItem>
+                <MenuItem value="french">French</MenuItem>
+                <MenuItem value="spanish">Spanish</MenuItem>
+            </Select>
+            <form className={classes.form}>
+                <FormControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="email">Email</InputLabel>
+                    <Input id="email" name="email" autoFocus />
+                </FormControl>
+                <FormControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <Input id="password" name="password" />
+                </FormControl>
+                <FormControlLabel control={<Checkbox color="primary" />} label="Remember Me" />
+                <Button
+                    className={classes.submit}
+                    variant="contained"
+                    type="submit"
+                    color="primary"
+                    fullWidth
+                >
+                    Sign In
+                </Button>
+            </form>
+        </Paper>
+    </main>
+
 export default withStyles(formStyles)(Form);
--- src/styles/FormStyles.js
+++ src/styles/FormStyles.js
@@ -1,15 +1,36 @@
 const formStyles = (theme) => ({
-  main: {
-    width: "auto",
-    display: "block",
-    marginLeft: theme.spacing(3),
-    marginRight: theme.spacing(3),
-    [theme.breakpoints.up("md")]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto",
+    main: {
+        width: "auto",
+        display: "block",
+        marginLeft: theme.spacing(3),
+        marginRight: theme.spacing(3),
+        [theme.breakpoints.up("sm")]: {
+            width: 400,
+            marginLeft: "auto",
+            marginRight: "auto",
+        },
     },
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: theme.spacing(2),
+        paddingRight: theme.spacing(3),
+        paddingBottom: theme.spacing(2),
+        paddingLeft: theme.spacing(3)
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%",
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        marginTop: theme.spacing(3)
+    }
 });
 
 export default formStyles;
